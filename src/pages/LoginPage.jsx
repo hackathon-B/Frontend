3a10697493fd4d1bed612249c3a8dca9b21f31ec
イズミ <izmk83@gmail.com>
2024-11-12 01:17:31 +0900

    react-cookie installed

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
 import { callApi } from '../api.js';
 
 function LoginPage() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+
+
+    async function login(email,password){
+        try {
+            const response = await callApi(post,login,[email,password])
+            //トークンを受け取ってstateに保存
+        } catch(error){
+            console.error(error)
+        }
+    }
+
     return (
         <>
-            <h1 className='text-blue-500'>hogehoge</h1>
             <div className="flex flex-col">
                 <label　for="email">Email</label>
-                <input id="email" type="email" placeholder="Email" />
+                <input id="email" type="email" placeholder="Email" onChange={e => setEmail(e.target.value)}/>
             </div>
             <div className="flex flex-row">
                 <label for="password">Password</label>
-                <input id="password" type="password" placeholder="Password" />
+                <input id="password" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
             </div>
             <div>
-                <button type="submit"> Login </button>
+                <button type="submit"　onclick={login(email,password)}> Login </button>
             </div>
         </>
     );
@@ -22,10 +35,3 @@ function LoginPage() {
 
 export default LoginPage;
 
-async function login(email,password){
-    try {
-        const response = await callApi(post,login,[email,password])
-    } catch(error){
-        console.error(error)
-    }
-}
